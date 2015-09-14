82cd5f28db787a099a9e2e12cfa19e3a94b39383
ruyar4 <ruyar410@gmail.com>
2015-09-14 10:31:45 -0300

    new changes

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+$( ".we-are" ).click(function() {
+  	$( ".show-we-are" ).slideToggle( "slow" );
+});
+
+$( ".vision" ).click(function() {
+  	$( ".show-vision" ).slideToggle( "slow" );
+});
+
+$( ".why" ).click(function() {
+  	$( ".show-why" ).slideToggle( "slow" );
+});
+
+$( ".what" ).click(function() {
+  	$( ".show-what" ).slideToggle( "slow" );
+});
+
+$( ".contact" ).click(function() {
+  	$( ".show-contact" ).slideToggle( "slow" );
+});
