c63481295aba5feea5972344cbea259f49e9fa0e
ruyar4 <ruyar410@gmail.com>
2015-09-27 20:01:14 -0300

    change color of text and add function to some buttons

--- js/javascript.js
+++ js/javascript.js
@@ -17,3 +17,15 @@ $( ".what" ).click(function() {
 $( ".contact" ).click(function() {
   	$( ".show-contact" ).slideToggle( "slow" );
 });
+
+$("#information").click(function() {
+	$(".master-column").fadeOut('slow', function() {
+		$(".data").css('width','100%');
+	});
+})
+
+$(".back-home").click(function () {
+	$(".master-column").fadeIn('slow', function () {
+		$(".data").css('width','25%');
+	})
+})
